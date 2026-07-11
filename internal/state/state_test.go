2853b7c582a5c2af700bdba4f167f8d68be06d20
Jacek Królikowski <nietaki@gmail.com>
2026-07-11 03:10:53 +0200

    state loading and saving

--- internal/state/state_test.go
+++ internal/state/state_test.go
@@ -1,6 +1,12 @@
 package state_test
 
 import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+
 	"github.com/google/uuid"
 	"github.com/mvrahden/go-test/pkg/gotest"
 	"github.com/nietaki/stingray/internal/state"
@@ -19,7 +25,16 @@ type SampleState struct {
 }
 
 func (s *StateManagerTestSuite) BeforeEach(t *gotest.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	dirname := filepath.Base(filename)
+	os.Chdir(path.Join(dirname, "../.."))
+
 	s.name = uuid.NewString()
+}
+
+func (s *StateManagerTestSuite) AfterEach(t *gotest.T) {
+	os.Remove("state/SampleState.json")
+	os.Remove(fmt.Sprintf("state/%s.json", s.name))
 }
 
 func (s *StateManagerTestSuite) TestHashing(t *gotest.T) {
@@ -51,5 +66,22 @@ func (s *StateManagerTestSuite) TestPersistance(t *gotest.T) {
 		gotest.Equal(it, 0, sm.Data.Int)
 		sm.LoadOrDefault(ss)
 		gotest.Equal(it, 42, sm.Data.Int)
+	})
+
+	t.It("saves its data right", func(it *gotest.T) {
+		ss := SampleState{
+			Int: 42,
+		}
+		sm := state.NewStateManager[SampleState]()
+		sm.LoadOrDefault(ss)
+		gotest.True(it, sm.CheckChanged())
+		err := sm.Save()
+		gotest.NoError(it, err)
+
+		sm2 := state.NewStateManager[SampleState]()
+		gotest.Equal(it, 0, sm2.Data.Int)
+		err = sm2.Load()
+		gotest.NoError(it, err)
+		gotest.Equal(it, 42, sm2.Data.Int)
 	})
 }
